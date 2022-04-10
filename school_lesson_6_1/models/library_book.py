811b9e01c90b10a5ed183736e99c5723174a6cfe
Yurii Razumovskyi <GarazdCreation@gmail.com>
2022-04-10 15:06:27 +0300

    [IMP] school_lesson_6_1: add delete xml sample; add a sample to refer to an xml_id from python

--- school_lesson_6_1/models/library_book.py
+++ school_lesson_6_1/models/library_book.py
@@ -6,3 +6,13 @@ class LibraryBook(models.Model):
     _description = 'Library Books'
 
     name = fields.Char('Title')
+    reader_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Reader',
+    )
+
+    def action_assign_default(self):
+        self.ensure_one()
+        self.reader_id = self.env.ref(
+            'school_lesson_6_1.res_partner_customer').id
+
