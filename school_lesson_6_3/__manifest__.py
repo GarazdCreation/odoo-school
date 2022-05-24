356101973d16a701e05bea196de99975a7ce8edc
Yurii Razumovskyi <GarazdCreation@gmail.com>
2022-05-24 18:35:41 +0300

    [ADD] school_lesson_6_3

---
+++ school_lesson_6_3/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright © 2022 Garazd Creation (<https://garazd.biz>)
+# @author: Yurii Razumovskyi (<support@garazd.biz>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+{
+    'name': 'School Lesson 6 3',
+    'version': '15.0.0.0.1',
+    'category': 'Extra Tools',
+    'summary': """
+        Odoo School
+        Lesson 6-3: Translating. 
+    """,
+    'license': 'OPL-1',
+    'author': 'Garazd Creation',
+    'website': 'https://garazd.biz',
+    'depends': [
+        'school_lesson_6_2',
+    ],
+    'data': [
+    ],
+    'support': 'support@garazd.biz',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
