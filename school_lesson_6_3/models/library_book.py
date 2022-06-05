6aba663afb5f90f6580c72e7c81779f528cf76d0
Yurii Razumovskyi <GarazdCreation@gmail.com>
2022-06-05 19:04:02 +0300

    [IMP] school_lesson_6_3: add translate method samples _, _lt

--- school_lesson_6_3/models/library_book.py
+++ school_lesson_6_3/models/library_book.py
@@ -1,7 +1,16 @@
-from odoo import fields, models
+from odoo import fields, models, _lt
+from odoo.exceptions import UserError
+
+ERR_MSG = _lt("You can't lend a book in advance.")
 
 
 class LibraryBook(models.Model):
     _inherit = 'library.book'
 
     description = fields.Text(translate=True)
+
+    def write(self, vals):
+        if vals.get('taken_date') and \
+                vals.get('taken_date') > fields.Date.today():
+            raise UserError(ERR_MSG)
+        return super(LibraryBook, self).write(vals)
