ec34100a54c74cf216e899e01dcca6d653349a5d
Yurii Razumovskyi <GarazdCreation@gmail.com>
2022-06-11 18:09:24 +0300

    [IMP] school_lesson_6_4: add ordered test method

--- school_lesson_6_4/tests/test_access_rights.py
+++ school_lesson_6_4/tests/test_access_rights.py
@@ -6,9 +6,16 @@ from odoo.exceptions import AccessError
 @tagged('post_install', '-at_install', 'library', 'access')
 class TestAccessRights(TestCommon):
 
-    def test_library_user_access_rights(self):
+    def test_01_library_user_access_rights(self):
         with self.assertRaises(AccessError):
             self.env['library.book'].with_user(self.library_user).create(
                 {'name': 'Test Book'})
         with self.assertRaises(AccessError):
             self.book_demo.with_user(self.library_user).unlink()
+
+    def test_02_library_admin_access_rights(self):
+        book = self.env['library.book'].with_user(self.library_admin).create(
+            {'name': 'Test Book'})
+        book.with_user(self.library_admin).read()
+        book.with_user(self.library_admin).write({'name': 'Test Book II'})
+        book.with_user(self.library_admin).unlink()
