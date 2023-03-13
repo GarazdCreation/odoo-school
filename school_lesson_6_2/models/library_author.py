f5b971cbdce39a66bb56aff6e694b6d986123b58
Mykola Ostroukh <koluchiy.nick@gmail.com>
2023-03-13 17:24:04 +0200

    [IMP] school_lesson_6_2: added functionality for the task of block 6.2

--- school_lesson_6_2/models/library_author.py
+++ school_lesson_6_2/models/library_author.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from odoo import fields, models
 
 
@@ -8,10 +10,11 @@ class LibraryAuthor(models.Model):
     first_name = fields.Char(required=True)
     last_name = fields.Char(required=True)
     birth_date = fields.Date('Birthday')
+    is_under_30_day = fields.Boolean(compute='_is_under_30_day')
 
     def name_get(self):
-        return [(rec.id, "%s %s" % (
-            rec.first_name, rec.last_name)) for rec in self]
+        return [(rec.id, "%s %s" % (rec.first_name, rec.last_name))
+                for rec in self]
 
     def action_delete(self):
         self.ensure_one()
@@ -20,3 +23,8 @@ class LibraryAuthor(models.Model):
 
     def _create_by_user(self, vals):
         return self.sudo().create(vals)
+
+    def _is_under_30_day(self):
+        for rec in self:
+            rec.is_under_30_day = rec.create_date > datetime.now() - timedelta(
+                days=30)
