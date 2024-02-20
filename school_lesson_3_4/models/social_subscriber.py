476622239f4ce184afecd62e84a0ddabe2a5a12d
Yurii Razumovskyi <GarazdCreation@gmail.com>
2024-02-20 23:13:22 +0200

    [IMP] school_lesson_3_4

--- school_lesson_3_4/models/social_subscriber.py
+++ school_lesson_3_4/models/social_subscriber.py
@@ -8,12 +8,14 @@ class SocialSubscriber(models.Model):
     def _selection_target_model(self):
         return [(model.model, model.name) for model in self.env['ir.model'].sudo().search([])]
 
+    # Many2one
     bank_id = fields.Many2one(
         comodel_name='res.bank',
         string='Local Bank',
         ondelete='restrict',
         help='The main bank of the subscriber.',
     )
+    # Many2many
     language_ids = fields.Many2many(
         comodel_name='res.lang',
         relation='social_subscriber_res_lang_rel',
@@ -21,6 +23,7 @@ class SocialSubscriber(models.Model):
         column2='language_id',
         string='Known Languages',
     )
+    # One2many
     promo_ids = fields.One2many(
         comodel_name='social.promo',
         inverse_name='subscriber_id',
@@ -34,7 +37,7 @@ class SocialSubscriber(models.Model):
     # Many2oneReference
     res_model_id = fields.Many2one('ir.model', 'Model', index=True, ondelete='cascade')
     res_model = fields.Char(string='Model Name', related='res_model_id.model', store=True, index=True, readonly=True)
-    res_id = fields.Many2oneReference(string='Resource ID', model_field='res_model', required=True, index=True)
+    res_id = fields.Many2oneReference(string='Resource ID', model_field='res_model', index=True)
     resource_ref = fields.Reference(
         string='Resource Ref',
         selection='_selection_target_model',
