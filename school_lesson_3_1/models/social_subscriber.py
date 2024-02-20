e9ff4a4a033dc1a78551c031d1aab5f5843cbbb6
Yurii Razumovskyi <GarazdCreation@gmail.com>
2024-02-20 22:37:42 +0200

    [IMP] school_lesson_3_1

--- school_lesson_3_1/models/social_subscriber.py
+++ school_lesson_3_1/models/social_subscriber.py
@@ -4,10 +4,12 @@ from odoo import fields, models
 class SocialSubscriber(models.Model):
     _name = 'social.subscriber'
     _inherits = {'res.partner': 'partner_id'}
+    _description = 'Subscribers from Social Networks'
 
     partner_id = fields.Many2one(
         comodel_name='res.partner',
         ondelete='cascade',
+        required=True,
         index=True,
     )
     social_service = fields.Selection(
