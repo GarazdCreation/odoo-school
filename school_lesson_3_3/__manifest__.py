99dde2b36e61faa6b5b04500d1196f8d9b200954
Yurii Razumovskyi <GarazdCreation@gmail.com>
2024-02-20 19:04:29 +0200

    [ADD] school_lesson_3_3

---
+++ school_lesson_3_3/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright © 2024 Garazd Creation (https://garazd.biz)
+# @author: Yurii Razumovskyi (support@garazd.biz)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+{
+    'name': 'School Lesson 3-3: Field Types',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """Odoo School Lesson 3-3: Model Field Types in Odoo. Reserved and special fields.""",
+    'license': 'LGPL-3',
+    'author': 'Garazd Creation',
+    'website': 'https://garazd.biz',
+    'depends': [
+        'school_lesson_3_2',
+    ],
+    'data': [
+        'views/social_subscriber_views.xml',
+    ],
+    'support': 'support@garazd.biz',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ school_lesson_3_3/models/social_subscriber.py
@@ -0,0 +1,56 @@
+from odoo import fields, models
+
+
+class SocialSubscriber(models.Model):
+    _inherit = 'social.subscriber'
+
+    sequence = fields.Integer(default=100)
+    nickname = fields.Char(size=8, trim=False, translate=False)
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('registered', 'Registered'),
+        ],
+        string='Status',
+        default='new',
+    )
+    is_confirmed = fields.Boolean(string='Contact is Confirmed')
+    client_bonus = fields.Monetary(
+        string='Bonus Amount',
+        currency_field='client_currency_id',
+        digits=(16, 2),
+        readonly=True,
+    )
+    notes = fields.Text(translate=True)
+    profile = fields.Html(sanitize=True)
+    passport = fields.Binary(attachment=True)
+    photo = fields.Image(
+        max_width=100,
+        max_height=100,
+        verify_resolution=True,
+    )
+    social_service = fields.Selection(
+        selection_add=[
+            ('facebook', 'Facebook'),
+        ],
+        ondelete={
+            # 'facebook': 'cascade',
+            # 'facebook': 'set default',
+            # 'facebook': 'set null',
+            # 'facebook': 'set undefined',
+            'facebook': lambda recs: recs.write({'social_service': 'undefined'}),
+        },
+        # The actions can be any of the following:
+        #   - 'set null' -- the default, all records with this option will have their selection value set to False.
+        #   - 'cascade' -- all records with this option will be deleted along with the option itself.
+        #   - 'set default' -- all records with this option will be set to the default of the field definition
+        #   - 'set VALUE' -- all records with this option will be set to the given value
+        #   - <callable> -- a callable whose first and only argument will be the set of records containing the
+        #     specified Selection option, for custom processing
+    )
+    start_date = fields.Date(string='Started')
+    end_date = fields.Date(string='Finished')
+
+
+
+
